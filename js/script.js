1ce5783a5a09c208d8145fdd2b4e8b010b77a799
M0hamm27d <s202277540@kfupm.edu.sa>
2026-04-18 13:12:24 +0300

    Changing auto validation to manual validation

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@ const header = document.querySelector('#about-me');
 const greeting = document.createElement('p');
 
 //Setting text based on time 
-if (hour < 12 ){
+if (hour < 12) {
     greeting.textContent = "Good morning";
 }
-else if (hour < 18){
+else if (hour < 18) {
     greeting.textContent = "Good afternoon";
 }
-else{
+else {
     greeting.textContent = "Good evening";
 }
 
@@ -28,18 +28,18 @@ const toggleButton = document.getElementById("theme-toggle");
 const savedTheme = localStorage.getItem("theme");
 
 //Applying Theme on Page Load
-if(savedTheme ==="dark"){
+if (savedTheme === "dark") {
     document.body.classList.add("dark-theme")
 }
 //Toggling theme on click --> when clicked run this function
-toggleButton.addEventListener("click",function(){
+toggleButton.addEventListener("click", function () {
     document.body.classList.toggle("dark-theme"); //It flips between states for body
     //Saving current theme
-    if(document.body.classList.contains("dark-theme")){
-        localStorage.setItem("theme","dark");
+    if (document.body.classList.contains("dark-theme")) {
+        localStorage.setItem("theme", "dark");
     }
-    else{
-        localStorage.setItem("theme","light");
+    else {
+        localStorage.setItem("theme", "light");
     }
 });
 
@@ -50,10 +50,10 @@ const filterButtons = document.querySelectorAll('.filter-btn');
 const projects = document.querySelectorAll('.project');
 
 //Loop through buttons
-filterButtons.forEach(function(button) {
-    button.addEventListener('click', function() {
+filterButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
         //Remove active class from all buttons
-        filterButtons.forEach(function(btn){
+        filterButtons.forEach(function (btn) {
             btn.classList.remove('active');
         });
         //Add active class to the clicked button
@@ -61,7 +61,7 @@ filterButtons.forEach(function(button) {
         //Getting the filter value from the button's data attribute
         const filterValue = button.getAttribute('data-filter');
 
-        projects.forEach(function(project) {
+        projects.forEach(function (project) {
             //Getting the category of the project from its data attribute
             const category = project.getAttribute('data-category');
             //Show project if filter is 'all' or matches the project's category
@@ -88,13 +88,13 @@ const messageInput = document.querySelector('#message');
 const formMessage = document.querySelector('#form-message');
 
 //Listening for submit event
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function (event) {
 
     //Prevent page refresh
     event.preventDefault();
 
     //Check inputs
-    if(nameInput.value === '' || emailInput.value === '' || messageInput.value === ''){
+    if (nameInput.value === '' || emailInput.value === '' || messageInput.value === '') {
 
         formMessage.textContent = "Please fill in all fields.";
         formMessage.style.color = "red";
@@ -102,7 +102,15 @@ form.addEventListener('submit', function(event){
         formMessage.scrollIntoView({ behavior: 'smooth', block: 'center' });
 
     }
-    else{
+    else if (!emailInput.checkValidity()) {
+
+        formMessage.textContent = "Please enter a valid email address , a valid email address must follow this pattern: example@example.com";
+        formMessage.style.color = "red";
+        formMessage.style.display = "block";
+        formMessage.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+    }
+    else {
 
         formMessage.textContent = "Message sent successfully!";
         formMessage.style.color = "green";
